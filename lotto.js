6d6f501f34afab868a617c2693bb233e6f6c9f39
Hoyoung Jung <honux77@gmail.com>
2022-09-02 09:38:44 +0900

    Fix bug

--- lotto.js
+++ lotto.js
@@ -5,7 +5,7 @@ for (var i = 1; i <= 45; i++) {
 }
 
 function shuffle() {
-  for (let i = 44; i >= 0; i--) {
+  for (let i = 44; i >= 2; i--) {
     const j = (parseInt(Math.random() * 4321) % i);
     const temp = a[i];
     a[i] = a[j];
@@ -15,9 +15,14 @@ function shuffle() {
 
 function lotto() {
   shuffle();
+  console.log(a)
   const l = a.slice(0, 6);
   l.sort((a, b) => a - b);
   return l;
+}
+
+for (let i = 0; i < 50; i++) {
+  console.log(lotto())
 }
 
 module.exports.lotto = lotto;
